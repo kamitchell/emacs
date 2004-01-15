cb46387ab6b4e4ba994d4018f071b4caabcc57ba
Kenichi Handa <handa@m17n.org>
2004-01-15 06:58:07 +0000

    (x_set_mouse_face_gc): Call FACE_FOR_CHAR with POS and OBJECT args.

--- src/macterm.c
+++ src/macterm.c
@@ -1649,9 +1649,9 @@ x_set_mouse_face_gc (s)
     face = FACE_FROM_ID (s->f, MOUSE_FACE_ID);
 
   if (s->first_glyph->type == CHAR_GLYPH)
-    face_id = FACE_FOR_CHAR (s->f, face, s->first_glyph->u.ch);
+    face_id = FACE_FOR_CHAR (s->f, face, s->first_glyph->u.ch, -1, Qnil);
   else
-    face_id = FACE_FOR_CHAR (s->f, face, 0);
+    face_id = FACE_FOR_CHAR (s->f, face, 0, -1, Qnil);
   s->face = FACE_FROM_ID (s->f, face_id);
   PREPARE_FACE_FOR_DISPLAY (s->f, s->face);
 
@@ -8827,6 +8827,3 @@ command, this enables the Mac keyboard to be used to enter non-ASCII
 characters directly.  */);
   mac_keyboard_text_encoding = kTextEncodingMacRoman;
 }
-
-/* arch-tag: f2259165-4454-4c04-a029-a133c8af7b5b
-   (do not change this comment) */
